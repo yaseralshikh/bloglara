973fb6f74563a489daa198530075b4fd8fc60983
yaser mohammad alshikh <yaseralshikh@hotmail.com>
2025-07-24 00:17:24 +0300

    Reorder navigation links to ensure 'Home' is displayed for all users

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,14 +22,14 @@
                         <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                             {{ __('Admin Dashboard') }}
                         </x-nav-link>
-                        <x-nav-link :href="route('home')" :active="request()->routeIs('Home')">
-                            {{ __('Home') }}
-                        </x-nav-link>
                     @else
                         <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                             {{ __('Dashboard') }}
                         </x-nav-link>
                     @endif
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('Home')">
+                        {{ __('Home') }}
+                    </x-nav-link>
                 </div>
             </div>
 
